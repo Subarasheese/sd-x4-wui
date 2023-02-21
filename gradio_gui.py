7b8d9b9b93bff4144779bbadacab7484a5b82e8b
Subarasheese <35178075+Subarasheese@users.noreply.github.com>
2023-02-21 07:20:51 -0300

    Update gradio_gui.py

--- gradio_gui.py
+++ gradio_gui.py
@@ -3,9 +3,9 @@ import upscaler
 import os
 from PIL import Image
 
-def upscale_image(prompt, rows=3,seed=0, image=None,xformers_input=False,cpu_offload_input=False,attention_slicing_input=False):
+def upscale_image(prompt,negative_prompt, rows=3,seed=0, image=None,enable_custom_sliders=False,guidance=7,iterations=50,xformers_input=False,cpu_offload_input=False,attention_slicing_input=False):
     cols = rows
-    output_image = upscaler.upscale_image(image, int(rows), int(cols),int(seed), prompt,xformers_input,cpu_offload_input,attention_slicing_input)
+    output_image = upscaler.upscale_image(image, int(rows), int(cols),int(seed), prompt,negative_prompt,xformers_input,cpu_offload_input,attention_slicing_input,enable_custom_sliders,guidance,iterations)
     output_image_path = "result.png"
     output_image.save(output_image_path)
 
@@ -14,21 +14,27 @@ def upscale_image(prompt, rows=3,seed=0, image=None,xformers_input=False,cpu_off
 
 image_input = gr.inputs.Image(label="Input Image")
 prompt_input = gr.inputs.Textbox(default="8k, photography, cgi, unreal engine, octane render, best quality",label="Prompt")
+negative_prompt_input = gr.inputs.Textbox(default="jpeg artifacts, lowres, bad quality",label="Negative prompt")
 seed_input = gr.inputs.Number(default=-1, label="Seed")
-row_input = gr.inputs.Number(default=3, label="Tile grid dimension amount (number of rows and columns) - v x v")
-xformers_input = gr.inputs.Checkbox(default=False,label="Enable Xformers memory efficient attention")
-cpu_offload_input = gr.inputs.Checkbox(default=False,label="Enable sequential CPU offload")
-attention_slicing_input = gr.inputs.Checkbox(default=False,label="Enable attention slicing")
+row_input = gr.inputs.Number(default=1, label="Tile grid dimension amount (number of rows and columns) - v x v")
+xformers_input = gr.inputs.Checkbox(default=True,label="Enable Xformers memory efficient attention")
+enable_custom_sliders = gr.inputs.Checkbox(default=False,label="(NOT RECOMMENDED) Click to enable the options below; if unchecked, it will ignore them and use the default settings")
+cpu_offload_input = gr.inputs.Checkbox(default=True,label="Enable sequential CPU offload")
+attention_slicing_input = gr.inputs.Checkbox(default=True,label="Enable attention slicing")
 output_image = gr.outputs.Image(label="Output Image",type='pil')
-
-
+guidance = gr.Slider(2, 15, 7, step=1, label='Guidance Scale: How much the AI influences the Upscaling.')
+iterations = gr.Slider(10, 75, 50, step=1, label='Number of Iterations')
 #save_png_button, save_png_halfsize_button ; I don't know how to implement them
 save_png_button = gr.Button(label="Save as a PNG image") # Added this button with the save_png function
 save_png_halfsize_button = gr.Button(label="Save as a PNG image (half size)") # Added this button with the save_png_halfsize function
 
 gr.Interface(fn=upscale_image,
-             inputs=[prompt_input,row_input,
-                     seed_input,image_input,
+             inputs=[prompt_input,negative_prompt_input,row_input,
+                     seed_input,
+                     image_input,
+                     enable_custom_sliders,
+                     guidance, 
+                     iterations,
                      xformers_input,
                      cpu_offload_input,
                      attention_slicing_input],
